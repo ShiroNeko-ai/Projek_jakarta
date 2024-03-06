ff85abfb15203b13d7e0181b3ab1c14c6eaf13ee
Amnapp <67993450+ShiroNeko-ai@users.noreply.github.com>
2024-03-06 09:51:08 +0700

    Update Print_nama.py

--- Print_nama.py
+++ Print_nama.py
@@ -1,1 +1,8 @@
 print ("Nama Kelompok Jakarta")
+
+print("Beni Ardiyanto")
+print("Kirana Andriyani")
+print("Muhammad Rayhan Denzuko")
+print("Satya Ananta Azis")
+print("Fransiscus Dharma Hadi Prayoga")
+print("Muhammad Faruoq Siswanto")
